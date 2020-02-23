a55b2d0fc046eb9d8acce3f52ae7c388fc1f2334
mihirpatel <mihirpatel948@gmail.com>
2020-02-23 10:17:25 +0530

    create routes for login signup dashboard admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'sign-up',
+    component: SignUpComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
